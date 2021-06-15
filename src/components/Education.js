1bd02bf374b5d0288e0c78a9b3f7e100990c757f
Souvik Banerjee <3125253+souviiik@users.noreply.github.com>
2021-06-15 18:40:30 +0530

    PersonalDetails and Education component skeleton

---
+++ src/components/Education.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Education() {
+  return (
+    <div>
+      <h2>Education</h2>
+      <ul>
+        <li>Course / Degree</li>
+        <li>School / University</li>
+        <li>Score</li>
+        <li>Passing Year</li>
+      </ul>
+      <button>Add</button>
+    </div>
+  )
+}
---
+++ src/components/PersonalDetails.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+export default function PersonalDetails() {
+  return (
+    <div>
+      <h2>Personal Details</h2>
+      <ul>
+        <li>Name</li>
+        <li>Address</li>
+        <li>Email</li>
+        <li>Phone</li>
+        <li>Photo</li>
+        <li>Date of Birth</li>
+        <li>Nationality</li>
+        <li>Marital Status</li>
+        <li>Website</li>
+        <li>Github</li>
+        <li>Linkedin</li>
+        <li>Facebook</li>
+        <li>Twitter</li>
+        <li>Medium</li>
+        <li>Stackoverflow</li>
+      </ul>
+    </div>
+  );
+}

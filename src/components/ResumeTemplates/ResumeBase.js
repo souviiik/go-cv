d1b4cf22ca2a8fc939ff9e5b3f1eb496bab4f378
Souvik Banerjee <3125253+souviiik@users.noreply.github.com>
2021-08-11 18:51:20 +0530

    adding icon to create resume button

--- src/components/ResumeTemplates/ResumeBase.js
+++ src/components/ResumeTemplates/ResumeBase.js
@@ -7,17 +7,17 @@ const ResumeBase = () => {
   const { name } = useParams();
 
   return (
-    <div id="section-to-print" style={{ width: "8.5in", margin: "0 auto" }}>
-      <div className="container">
-        <h2 className="title is-size-2">Candid</h2>
+    <div className="container">
+      <h2 className="title is-size-2">Candid</h2>
+      <div id="section-to-print" style={{ width: "100%", margin: "0 auto" }}>
         {name === "candid" && <Candid />}
-        <br />
-        <hr />
-        <br />
-        <button className="button is-link" onClick={() => window.print()}>
-          <FaPrint className="mr-2" /> Print
-        </button>
       </div>
+      <br />
+      <hr />
+      <br />
+      <button className="button is-link" onClick={() => window.print()}>
+        <FaPrint className="mr-2" /> Print
+      </button>
     </div>
   );
 };
